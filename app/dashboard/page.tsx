74158ef84f12c95384eae76be6eb48e59035584d
glody007 <glodymbutwile@gmail.com>
2023-05-25 15:47:08 +0200

    feat: bar chart.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,12 @@
 import IncomePerYear from "@/components/chart/income-per-year"
+import SalePerYear from "@/components/chart/sale-per-year"
 import Notifications from "@/components/notifications"
 
 export default function Home() {
   return (
     <div className="grid grid-rows-6 grid-cols-4 gap-4">
         <div className="row-span-2 rounded-xl h-60">
-            
+            <SalePerYear />
         </div>
         <div className="row-span-2 bg-indigo-600 rounded-xl">02</div>
         <div className="bg-indigo-600 rounded-xl">03</div>
--- types/index.d.ts
+++ types/index.d.ts
@@ -3,5 +3,6 @@ export {};
 declare global {
   interface Window {
     myLine: any;
+    myBar: any;
   }
 }
