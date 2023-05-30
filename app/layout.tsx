23756822d64ecb7ec9ef99f9f4c93e4d792f185d
glody007 <glodymbutwile@gmail.com>
2023-05-30 14:33:12 +0200

    chore: Setup toast.
    react-hot-toast and shadcn toast.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import QueryWrapper from './QueryWrapper'
+import { Toaster } from 'react-hot-toast'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <QueryWrapper>
+          <Toaster />
           {children}
         </QueryWrapper>
       </body>
