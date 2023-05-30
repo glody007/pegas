12e2e199bb6904bb09fab511d9cf97fe99927e64
glody007 <glodymbutwile@gmail.com>
2023-05-30 07:42:32 +0200

    feat: Loading state and dashboard navigation.

--- app/auth/Logged.tsx
+++ app/auth/Logged.tsx
@@ -1,13 +1,38 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { Loader2 } from 'lucide-react'
 import {signOut} from 'next-auth/react'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation';
 
 export default function Logged(){ 
-    
+    const router = useRouter() 
+    const [loading, setLoading] = useState(false)
+
+    const logout = async () => {
+        setLoading(true)
+        signOut()
+            .catch((error) => {
+                console.log(error)
+            })
+            .finally(() => {
+                setLoading(false)
+            }) 
+    }
+
     return (
-        <Button variant="outline" onClick={() => signOut()}>
-            Sign out
-        </Button>
+        <div className="flex space-x-4">
+            <Button 
+                disabled={loading}
+                variant="outline" onClick={() => logout()}
+            >
+                {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                Sign out
+            </Button>
+            <Button onClick={() =>  router.push('/dashboard')}>
+                Dashboard
+            </Button>
+        </div>
     )
 }
--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -1,12 +1,35 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { Loader2 } from 'lucide-react';
 import {signIn} from 'next-auth/react'
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
-export default function Login(){ 
+export default function Login(){
+    const router = useRouter() 
+    const [loading, setLoading] = useState(false)
+
+    const login = async () => {
+        setLoading(true)
+        signIn()
+            .then((value) => {
+                console.log(value)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }
     
     return (
-        <Button onClick={() => signIn()}>
+        <Button 
+            onClick={() => login()}
+            disabled={loading}
+        >
+            {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
             Sign In
         </Button>
     )
