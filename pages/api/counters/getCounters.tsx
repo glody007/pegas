4d812c5867003e153e94a2088c5cb952d75f85c3
glody007 <glodymbutwile@gmail.com>
2023-06-01 10:33:09 +0200

    feat: Fetch counters.

---
+++ pages/api/counters/getCounters.tsx
@@ -0,0 +1,28 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+import prisma from "@/prisma/client";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const session = await getServerSession(req, res, authOptions)
+    if(req.method === "GET") {
+        try {
+            const data = await prisma.counter.findMany()
+            res.status(200).json({
+                success: true,
+                code: 200,
+                data: data
+            })
+        } catch(err) {
+            console.log(err)
+            res.status(403).json({
+                success: false,
+                code: 403,
+                errors: [{ message: "Error fetching buses" }]
+            })
+        }  
+    }
+}
