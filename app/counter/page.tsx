74bbf7df9b7237782cf024d491d951a7215a9fde
glody007 <glodymbutwile@gmail.com>
2023-06-03 09:55:40 +0200

    fix: Ticket list and home page content.

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -1,9 +1,11 @@
+import ScheduleList from "./ScheduleList"
+
 export default async function Page() {
 
   return (
     <>
       <div className="mx-auto mt-4">
-        
+        <ScheduleList />
       </div>
     </>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import Nav from './auth/Nav'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div>
-        {/* @ts-expect-error Server Component */}
-        <Nav />
+       
       </div>
     </main>
   )
