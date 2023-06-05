ae9cd34cc5eaf8d689425219d2862f6c161bbab2
glody007 <glodymbutwile@gmail.com>
2023-06-05 08:19:46 +0200

    feat: Header and search bar.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,44 @@
+import SearchBar from "@/components/SearchBar";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import backroung from '../images/booking-background.jpg'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div>
-       
+    <main className="">
+      <div className="relative flex flex-col w-full justify-center items-center justify-center">
+
+        <div className="absolute top-0 left-0 w-full h-[460px]">
+          <Image fill objectFit="cover" src={backroung} alt="Background Image" />
+        </div>
+        <div className="absolute top-0 left-0 w-full h-[460px] bg-blue-600 opacity-80"></div>
+
+        <div className="flex flex-col justify-between w-[1200px] z-50">
+          <div className="flex justify-between py-4">
+            <Link href="/"> 
+              <div className="font-bold text-white p-2 rounded-lg bg-blue-600">
+                LA PATIENCE
+              </div>
+            </Link>
+            <div className="">
+              <Link href="/feature"> 
+                <Button variant="secondary">Dashboard</Button>
+              </Link>
+            </div>
+          </div>
+          <div className="mt-24">
+            <h1 className="text-5xl text-white font-extrabold">
+              Plus besoin de faire la queue
+            </h1>
+            <p className="text-3xl font-thin mt-4 text-white">
+              Réserver votre billet de bus en ligne.
+            </p>
+          </div>
+          <div className="flex w-full justify-center mt-16">
+            <SearchBar />
+          </div>
+        </div>
       </div>
     </main>
   )
---
+++ components/SearchBar.tsx
@@ -0,0 +1,156 @@
+"use client"
+
+import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+import { format } from "date-fns"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Combobox, comboboxItem } from './ui/combobox'
+import { Input } from "@/components/ui/input"
+import { useForm } from 'react-hook-form'
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { CalendarIcon } from "lucide-react"
+import { Calendar } from "@/components/ui/calendar"
+import { cn } from '@/lib/utils'
+import { CitySchema } from '@/lib/validators/city'
+
+const formSchema = z.object({
+  from: z.string().min(2, {
+    message: "From must be at least 2 characters.",
+  }),
+  to: z.string().min(2, {
+    message: "To must be at least 2 characters.",
+  }),
+  date: z.coerce.date(),
+})
+
+ 
+// 2. Define a submit handler.
+function onSubmit(values: z.infer<typeof formSchema>) {
+// Do something with the form values.
+// ✅ This will be type-safe and validated.
+console.log(values)
+}
+
+type Props = {}
+
+export default function SearchBar({}: Props) {
+  // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            from: "",
+            to: "",
+            date: new Date()
+        },
+    })
+
+  const cities = Object.values(CitySchema.Values).map(value => value)
+
+  return (
+    <div className="flex-1 p-8 bg-white rounded-xl">
+        <Form {...form}>
+            <form 
+                onSubmit={form.handleSubmit(onSubmit)} 
+                className="flex items-center space-x-2"
+            >
+                <div>
+                    <FormField
+                        control={form.control}
+                        name="from"
+                        render={({ field }) => (
+                            <FormItem>
+                                <Combobox 
+                                    placeholder="Où êtes-vous?" 
+                                    searchHint="Ville" 
+                                    items={cities.map((city, index) => ({
+                                        value: city,
+                                        label: city
+                                    }))}
+                                    handleSelect={field.onChange}
+                                />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <div>
+                    <FormField
+                        control={form.control}
+                        name="to"
+                        render={({ field }) => (
+                            <FormItem>
+                                <Combobox 
+                                    placeholder="Où allez-vous" 
+                                    searchHint="Ville" 
+                                    items={cities.map((city, index) => ({
+                                        value: city,
+                                        label: city
+                                    }))}
+                                    handleSelect={field.onChange}
+                                />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <div className="flex-[0.6]">
+                    <FormField
+                        control={form.control}
+                        name="date"
+                        render={({ field }) => (
+                            <FormItem>
+                                <Popover>
+                                    <PopoverTrigger asChild>
+                                    <FormControl>
+                                        <Button
+                                        variant={"outline"}
+                                        className={cn(
+                                            "w-full pl-3 text-left font-normal",
+                                            !field.value && "text-muted-foreground"
+                                        )}
+                                        >
+                                        {field.value ? (
+                                            format(field.value, "PPP")
+                                        ) : (
+                                            <span>CHOISIR LA DATE</span>
+                                        )}
+                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                        </Button>
+                                    </FormControl>
+                                    </PopoverTrigger>
+                                    <PopoverContent className="w-auto p-0" align="start">
+                                    <Calendar
+                                        mode="single"
+                                        selected={field.value}
+                                        onSelect={field.onChange}
+                                        disabled={(date) =>
+                                            date < new Date()
+                                        }
+                                        initialFocus
+                                    />
+                                    </PopoverContent>
+                                </Popover>
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <Button type="submit" className="flex-[0.6]">
+                    Recherche
+                </Button>
+            </form>
+        </Form>
+    </div>
+  )
+}
