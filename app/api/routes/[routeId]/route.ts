61c5293ca8c802053b923d43a4a03f39ee526b4d
glody007 <glodymbutwile@gmail.com>
2023-06-11 08:02:50 +0200

    feat: Update route endpoint.

--- app/api/routes/[routeId]/route.ts
+++ app/api/routes/[routeId]/route.ts
@@ -38,3 +38,64 @@ export async function GET(
         }, { status: 500 })
     }  
 }
+
+export async function PUT(
+    req: NextRequest,
+    { params }: { params: Query }
+) {
+    try {
+        const session = true //await getServerSession(req, res, authOptions)
+        if(!session) return NextResponse.json({
+            success: false,
+            code: 401,
+            errors: [{ message: "Please sign in" }]
+        }, { status: 401 })
+
+        const route: Route = await req.json()
+        
+        const validate = RouteSchema.safeParse(route)
+
+        if(!validate.success) {
+            return NextResponse.json({
+                success: false,
+                code: 403,
+                errors: validate.error.issues.map(issue => ({
+                    message: issue.message
+                }))
+            }, { status: 403 })
+        }
+
+        const exist = await prisma.route.findFirst({
+            where: {
+                from: route.from,
+                to: route.to
+            }
+        })
+
+        if(exist) {
+            return NextResponse.json({
+                success: false,
+                code: 403,
+                errors: [{ message: "Route existante" }]
+            }, { status: 403 })
+        }
+
+        const result = await prisma.route.update({
+            where: { id: params.routeId },
+            data: route
+        })
+
+        return NextResponse.json({
+            success: true,
+            code: 200,
+            data: result
+        }, { status: 200 })
+    } catch(err) {
+        console.log(err)
+        return NextResponse.json({
+            success: false,
+            code: 500,
+            errors: [{ message: "Error has occured while updating route" }]
+        }, { status: 500 })
+    }
+}
